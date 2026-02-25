69cc1fcc32f9efb430b89649e677bc3dea589826
Teetawatch <teetawatch.p@gmail.com>
2026-02-25 15:18:55 +0700

    Fix: Add fillable fields to TeacherEvaluation model to allow mass assignment

--- app/Models/TeacherEvaluation.php
+++ app/Models/TeacherEvaluation.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class TeacherEvaluation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'teacher_id',
+        'subject_id',
+        'semester',
+        'rating_knowledge',
+        'rating_method',
+        'rating_content_order',
+        'rating_motivation',
+        'rating_qa',
+        'rating_media',
+        'rating_documents',
+        'comment',
+        'problems_suggestions',
+    ];
 }
